488f6bfcb212785c42bcd985d6eb7bbe0166ad85
Daniel McKnight <daniel@neon.ai>
2025-02-06 14:19:24 -0800

    Update neon-mq-connector dependency to stable spec Update license notices to 2025

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
 # All trademark and other rights reserved by their respective owners
-# Copyright 2008-2024 Neongecko.com Inc.
+# Copyright 2008-2025 Neongecko.com Inc.
 # Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
 # Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
 # Mike Gray, David Scripka
